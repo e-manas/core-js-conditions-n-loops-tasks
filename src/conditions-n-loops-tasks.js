353b01823bb29445f20b664810933f9a9af3a999
Manas Egemberdiev <manas.egemberdiev@gmail.com>
2025-01-08 17:37:11 +0600

    resolve task isIsoscelesTriangle

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -87,8 +87,10 @@ function canQueenCaptureKing(queen, king) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  if (a + b <= c || a + c <= b || c + b <= a) return false;
+  if (a === b || a === c || c === b) return true;
+  return false;
 }
 
 /**
