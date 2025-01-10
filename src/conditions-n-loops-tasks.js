ac61ac59c87ecfd0948ec3cccae760e295ddffe5
Manas Egemberdiev <manas.egemberdiev@gmail.com>
2025-01-11 02:02:41 +0600

    resolve task getBalanceIndex

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -281,8 +281,24 @@ function isContainNumber(num, digit) {
  *  [2, 3, 9, 5] => 2       => 2 + 3 === 5 then balance element is 9 and its index = 2
  *  [1, 2, 3, 4, 5] => -1   => no balance element
  */
-function getBalanceIndex(/* arr */) {
-  throw new Error('Not implemented');
+function getBalanceIndex(arr) {
+  const totalSum = (array) => {
+    let sum = 0;
+    for (let i = 0; i < array.length; i += 1) {
+      sum += array[i];
+    }
+    return sum;
+  };
+  const total = totalSum(arr);
+  let leftSum = 0;
+  for (let i = 0; i < arr.length; i += 1) {
+    const rightSum = total - leftSum - arr[i];
+    if (leftSum === rightSum) {
+      return i;
+    }
+    leftSum += arr[i];
+  }
+  return -1;
 }
 
 /**
